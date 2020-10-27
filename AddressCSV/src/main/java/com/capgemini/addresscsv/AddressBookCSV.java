1b18bc0f2f4190ec19366d8ff3a880929fe5912f
batista12 <50904472+batista12@users.noreply.github.com>
2020-10-26 22:49:45 -0700

    [Manasi]AddressBookCSV main branch is created

---
+++ AddressCSV/src/main/java/com/capgemini/addresscsv/AddressBookCSV.java
@@ -0,0 +1,13 @@
+package com.capgemini.addresscsv;
+
+/**
+ * Hello world!
+ *
+ */
+public class AddressBookCSV 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
